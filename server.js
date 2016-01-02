6df24e567896bc7f0d64acd2f28645623db88a54
Ajay Kaushik <ajaykaushik4u@gmail.com>
2016-01-02 12:54:03 -0600

    Added main banner

--- server.js
+++ server.js
@@ -12,7 +12,7 @@
  app.set('port', process.env.PORT || 3000);
  app.set('views', __dirname + '/views');
  app.set('view engine', 'jade');
-
+ 
  app.use(morgan('dev'));
 
  app.use(bodyParser.urlencoded({
